8c119decbdffb14a2852410f5862657034ba4cb9
dnoblejas59 <ronperri89@gmail.com>
2024-11-30 18:06:13 +0100

    NavBar

---
+++ .idea/src/componentes/Navbar.svelte
@@ -0,0 +1,24 @@
+<script>
+    export let items = [];
+</script>
+
+<nav>
+    {#each items as item}
+        <a href="#">{item}</a>
+    {/each}
+</nav>
+
+<style>
+    nav {
+        display: flex;
+        justify-content: space-between;
+        background-color: green;
+        padding: 1rem;
+        color: white;
+    }
+    a {
+        color: white;
+        text-decoration: none;
+        margin: 0 1rem;
+    }
+</style>
